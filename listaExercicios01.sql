508117f67ae9fd4c967a23b1da4dfec4e28aa6d8
Ryan <ryanmoreira1112@gmail.com>
2023-09-04 00:39:37 -0300

    Determine o número total de matrículas para cada aluno.

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -196,3 +196,9 @@ FROM autores
 LEFT JOIN livros ON autores.id = livros.autor_id
 GROUP BY autores.nome;
 
+--Determine o número total de matrículas para cada aluno.
+SELECT alunos.nome, COUNT(matriculas.id) AS total_de_matriculas
+FROM alunos
+LEFT JOIN matriculas ON alunos.id = matriculas.aluno_id
+GROUP BY alunos.nome;
+

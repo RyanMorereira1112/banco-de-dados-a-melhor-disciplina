ec50096a50c98f1e591b48a7d8ac8b6bbd4f0de1
Ryan <ryanmoreira1112@gmail.com>
2023-09-26 17:58:25 -0300

    7. Adição de Livro com Tratamento de Erros

--- ListaExercícios02.sql
+++ ListaExercícios02.sql
@@ -260,3 +260,24 @@ END //
 DELIMITER ;
 
 CALL sp_TitulosPorCategoria('História');
+
+DELIMITER //
+CREATE PROCEDURE sp_AdicionarLivro(IN novoTitulo VARCHAR(255), IN novaEditoraID INT, IN novoAno INT, IN novoNumPaginas INT, IN novaCategoriaID INT, OUT mensagem VARCHAR(255))
+BEGIN
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+        ROLLBACK;
+        SET mensagem = 'Erro ao adicionar o livro. Verifique os dados e tente novamente.';
+    END;
+
+    START TRANSACTION;
+    INSERT INTO Livro (Titulo, Editora_ID, Ano_Publicacao, Numero_Paginas, Categoria_ID)
+    VALUES (novoTitulo, novaEditoraID, novoAno, novoNumPaginas, novaCategoriaID);
+    COMMIT;
+
+    SET mensagem = 'Livro adicionado com sucesso.';
+END //
+DELIMITER ;
+
+CALL sp_AdicionarLivro('Novo Livro', 1, 2023, 300, 2, @mensagem);
+SELECT @mensagem;

9fb14c7a0bd0e1268f2d5356666aadb70d3bd90a
Ryan <ryanmoreira1112@gmail.com>
2023-09-26 17:52:08 -0300

    4. Verificação de Livros por Categoria

--- ListaExercícios02.sql
+++ ListaExercícios02.sql
@@ -208,3 +208,18 @@ DELIMITER ;
 
 CALL sp_ContarLivrosPorCategoria('Ficção Científica', @total);
 SELECT @total;
+
+DELIMITER //
+CREATE PROCEDURE sp_VerificarLivrosCategoria(IN categoriaNome VARCHAR(100), OUT possuiLivros BOOLEAN)
+BEGIN
+    SELECT EXISTS (
+        SELECT 1
+        FROM Livro
+        INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+        WHERE Categoria.Nome = categoriaNome
+    ) INTO possuiLivros;
+END //
+DELIMITER ;
+
+CALL sp_VerificarLivrosCategoria('Ciência', @possuiLivros);
+SELECT @possuiLivros;

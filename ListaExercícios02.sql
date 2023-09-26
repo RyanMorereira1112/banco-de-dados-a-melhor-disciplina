8216c472f82e608251149b93db9587d33415d1e3
Ryan <ryanmoreira1112@gmail.com>
2023-09-26 17:47:41 -0300

    2. Livros por Categoria

--- ListaExercícios02.sql
+++ ListaExercícios02.sql
@@ -182,3 +182,16 @@ END //
 DELIMITER ;
 
 CALL sp_ListarAutores();
+
+DELIMITER //
+CREATE PROCEDURE sp_LivrosPorCategoria(IN categoriaNome VARCHAR(100))
+BEGIN
+    SELECT Livro.Titulo
+    FROM Livro
+    INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+    WHERE Categoria.Nome = categoriaNome;
+END //
+DELIMITER ;
+
+CALL sp_LivrosPorCategoria('Romance');
+CALL sp_LivrosPorCategoria('Ciência');
